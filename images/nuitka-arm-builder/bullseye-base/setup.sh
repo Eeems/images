00798da507ef5c8636f04a8219afe25a31d115d0
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-12-24 02:12:27 -0700

    Fix syntax error

--- images/nuitka-arm-builder/bullseye-base/setup.sh
+++ images/nuitka-arm-builder/bullseye-base/setup.sh
@@ -3,7 +3,7 @@ pyenv install "$1"
 lib_path="$(pyenv prefix "$1")/lib"
 # Hack to get --static-libpython=yes to work for nuitka
 ln -s \
-  "${lib_path}/python${1}/config-${1}-arm-linux-gnueabihf/libpython${1}.a"
+  "${lib_path}/python${1}/config-${1}-$(gcc -dumpmachine)/libpython${1}.a" \
   "${lib_path}/libpython${1}.a"
 pyenv global "$1"
 pyenv exec python -m venv /opt/lib/nuitka
