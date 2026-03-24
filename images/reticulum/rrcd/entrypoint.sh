10baf174eb219e29a3e477d1b83af80c4460d459
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-03-24 01:10:20 -0600

    Fix rrcd file locations

--- images/reticulum/rrcd/entrypoint.sh
+++ images/reticulum/rrcd/entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 if [ $# -ne 0 ]; then
   exec "$@"
 fi
-exec rrcd --config=/config
+exec rrcd \
+  --config="$RNS_CONFIG_PATH" \
+  --identity="$RNS_CONFIG_PATH"/hub_identity \
+  --room-registry="$RNS_CONFIG_PATH"/rooms.toml \
+  --hub-name="${RRCD_HUB_NAME:-RRC}"
