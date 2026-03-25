88a66801bb1efe6f8db0cd0da65bd156cb2e71f1
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-03-25 13:44:52 -0600

    Add rngit

---
+++ images/reticulum/rngit/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/ash
+set -e
+source /setup
+if [ $# -ne 0 ]; then
+  exec "$@"
+fi
+REPO_PATH=${REPO_PATH:-/data}
+echo "RNS_CONFIG_PATH: $RNS_CONFIG_PATH"
+echo "REPO_PATH: $REPO_PATH"
+ALLOW_ALL_READ=${ALLOW_ALL_READ:-0}
+flags=
+f() {
+  flags="$flags --allow-$1=$2"
+}
+if [[ "$ALLOW_ALL_READ" == "1" ]]; then
+  flags=--allow-all-read
+elif [[ "$ALLOW_READ" != "" ]]; then
+  while read -r i; do
+    f read "$i"
+  done < <(echo "$ALLOW_READ" | xargs)
+fi
+if [[ "$ALLOW_WRITE" != "" ]]; then
+  while read -r i; do
+    f write "$i"
+  done < <(echo "$ALLOW_WRITE" | xargs)
+fi
+if [[ "$ANNOUNCE_INTERVAL" != "" ]]; then
+  flags="$flags --announce-interval=$ANNOUNCE_INTERVAL"
+fi
+mkdir -p "$RNS_CONFIG_PATH"
+mkdir -p "$REPO_PATH"
+exec -a rngit \
+  rngit $flags --verbose "$REPO_PATH"
